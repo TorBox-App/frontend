a5f754d0c88c49f7fef77a7802b46727c7e9bb4b
David <25761442+Wamy-Dev@users.noreply.github.com>
2023-12-17 15:10:05 -0600

    feat: added proper assets and cleaned up boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Index from './index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>,
 )
